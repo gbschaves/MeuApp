43ed33d77abd61e241100323f549723e9f9427bb
Guilherme Chaves <gb.chaves@hotmail.com>
2023-03-27 09:41:55 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MeuApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Product Mouse = new Product(1, "Mouse Gamer", 29);
+
+      Console.WriteLine(Mouse.Id);
+      Console.WriteLine(Mouse.Name);
+      Console.WriteLine(Mouse.Price);
+    }
+  }
+
+  struct Product
+      {
+        public Product(int id, string name, double price)
+        {
+          Id = id;
+          Name = name;
+          Price = price;
+        }
+        
+        public int Id;
+        public string Name;
+        public double Price;
+        
+        public double PriceInDolar(double dolar)
+        {
+          return Price * dolar;
+        }
+      }
+
+}
