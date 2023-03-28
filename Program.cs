8cf8fd91c33742f58e4b8b209b96e05a0c3d60ba
Guilherme Chaves <gb.chaves@hotmail.com>
2023-03-28 13:03:39 -0300

    Calculator

--- Program.cs
+++ Program.cs
@@ -7,34 +7,109 @@ namespace MeuApp
   {
     static void Main(string[] args)
     {
-      Product Mouse = new Product(1, "Mouse Gamer", 29);
-
-      Console.WriteLine(Mouse.Id);
-      Console.WriteLine(Mouse.Name);
-      Console.WriteLine(Mouse.Price);
-      Console.WriteLine("Teste de sincronia com git");
-      
-      Product Teclado = new Product(2, "Teclado", 50);
+      Menu();
     }
+
+    static void Menu(){
+      Console.Clear();
+
+      Console.WriteLine("O que deseja fazer?");
+      Console.WriteLine("1 - Soma");
+      Console.WriteLine("2 - Subtração");
+      Console.WriteLine("3 - Multiplicação");
+      Console.WriteLine("4 - Media");
+      Console.WriteLine("-------------------");
+      Console.WriteLine("Selecione uma opção: ");
+
+      short res = short.Parse(Console.ReadLine());
+
+      switch(res){
+        case 1: Soma(); break;
+        case 2: Subtracao(); break;
+        case 3: Multiplicacao(); break;
+        case 4: Divisao(); break;
+        default: Menu(); break;
+
+      }
+    }
+
+    static void Soma()
+    {
+      Console.Clear();
+      Console.WriteLine("Primeiro Valor: ");
+      float v1 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Segundo Valor: ");
+      float v2 = float.Parse(Console.ReadLine());
+     
+     Console.WriteLine("");
+
+     float resultado = v1 + v2;
+
+    //  Console.WriteLine("O Resultado da soma é " + resultado);
+     Console.WriteLine($"O resultado da soma é {resultado}");
+    //  Console.WriteLine($"O resultado da soma é {v1 + v2}");
+    //  Console.WriteLine("O resultado da soma é " + (v1 + v2));
+     Console.ReadKey();
+     Menu();
+    }
+
+    static void Subtracao() {
+      Console.Clear();
+      Console.WriteLine("Primeiro Valor: ");
+      float v1 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Segundo Valor: ");
+      float v2 = float.Parse(Console.ReadLine());
+     
+     Console.WriteLine("");
+
+     float resultado = v1 - v2;
+
+     Console.WriteLine($"O resultado da soma é {resultado}");
+
+     Console.ReadKey();
+
+     Menu();
+    }
+
+    static void Divisao() {
+      Console.Clear();
+      Console.WriteLine("Primeiro Valor: ");
+      float v1 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Segundo Valor: ");
+      float v2 = float.Parse(Console.ReadLine());
+     
+     Console.WriteLine("");
+
+     float resultado = v1 / v2;
+
+     Console.WriteLine($"O resultado da soma é {resultado}");
+
+     Console.ReadKey();
+     Menu();
+    }
+
+    static void Multiplicacao() {
+      Console.Clear();
+      Console.WriteLine("Primeiro Valor: ");
+      float v1 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Segundo Valor: ");
+      float v2 = float.Parse(Console.ReadLine());
+     
+     Console.WriteLine("");
+
+     float resultado = v1 * v2;
+
+     Console.WriteLine($"O resultado da soma é {resultado}");
+
+     Console.ReadKey();
+     Menu();
+    }
+
   }
 
-  struct Product
-      {
-        public Product(int id, string name, double price)
-        {
-          Id = id;
-          Name = name;
-          Price = price;
-        }
-        
-        public int Id;
-        public string Name;
-        public double Price;
-        
-        public double PriceInDolar(double dolar)
-        {
-          return Price * dolar;
-        }
-      }
 
 }
